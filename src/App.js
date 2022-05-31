ff056d9068c6deb8110b66a3f4fed5770be0b9ba
SooOh <su@mail.i-on.net>
2022-05-31 10:12:10 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [per, setPer] = useState(0);
+  setInterval(function () {
+    const now = new Date().getTime();
+    const st = new Date("2022-05-31:09:30:00").getTime();
+    const et = new Date("2022-05-31:18:30:00").getTime();
+    setPer((((now - st) * 100) / (et - st)).toString().slice(0, 3) + "%");
+  }, 60000);
+  useEffect(() => {
+    const now = new Date().getTime();
+    const st = new Date("2022-05-31:09:30:00").getTime();
+    const et = new Date("2022-05-31:18:30:00").getTime();
+    setPer((((now - st) * 100) / (et - st)).toString().slice(0, 3) + "%");
+  }, []);
+  return <div className="App">{per}</div>;
 }
 
 export default App;
